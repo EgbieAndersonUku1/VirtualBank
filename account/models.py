fab48928819700e3c71735f160903af74d7c1b0a
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2025-06-30 23:20:20 +0100

    Feat add models
    - Added the profile model
    - Added the identification model
    
    Error
    - fixed the `current_year_choices` by adding `.now()` before adding the `.year`
    - fix the import in models by adding a `.utils.utils`

--- account/models.py
+++ account/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator
 
 from authentication.models import User
 from utils.generator import generate_code
-from account.utils import current_year_choices
+from .utils.utils import current_year_choices
 
 # Create your models here.
 
@@ -19,11 +19,12 @@ class BankAccount(models.Model):
 
     class Meta:
         indexes = [
-            models.indexes(fields=['sort_code', 'account_number'])
+            models.Index(fields=['sort_code', 'account_number'])
         ]
 
+
     @property
-    def account_number(self):
+    def full_account_number(self):
         return f"{self.sort_code}{self.account_number}"
     
     @property
@@ -49,26 +50,25 @@ class Card(models.Model):
         DEC = "DEC", "December"
 
     class CardOptions(models.TextChoices):
-        VISA        = "Visa",       "V"
-        MASTER_CARD = "MasterCard", "M"
-        DISCOVER    = "Discover",   "D"
+        VISA        = "V", "Visa"
+        MASTER_CARD = "M", "MasterCard"
+        DISCOVER    = "D", "Discover"
 
     class CardType(models.TextChoices):
-        CREDIT  = "Credit", "C"
-        DEBIT   = "Debit", "D"
+        CREDIT = "C", "Credit"
+        DEBIT  = "D", "Debit"
+
 
     card_name    = models.CharField(max_length=20)
     card_number  = models.CharField(max_length=16)
     expiry_month = models.CharField(choices=Month.choices, max_length=3)
     expiry_year  = models.PositiveBigIntegerField(choices=current_year_choices())
-    card_options = models.CharField(choices=CardOptions.choices, max_length=1)
-    card_type    = models.CharField(choices=CardOptions.choices, max_length=1)
+    card_options = models.CharField(choices=Month.choices, max_length=3)
+    card_type    = models.CharField(choices=CardType.choices, max_length=1)
     cvc          = models.CharField(max_length=3)
     account      = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name="cards")
 
 
-
-   
 
 class Wallet(models.Model):
     wallet_id             = models.CharField(default=generate_code(maximum_length=11), unique=True)
@@ -80,3 +80,53 @@ class Wallet(models.Model):
     maximum_cards         = models.SmallIntegerField(validators=[MinValueValidator(0)])
     created_on            = models.DateTimeField(auto_now_add=True)
     modified_on           = models.DateTimeField(auto_now=True)
+
+
+
+class Profile(models.Model):
+
+    class Gender(models.TextChoices):
+        SELECT_GENDER = "", "Select your gender"
+        MALE          = "M", "Male"
+        FEMALE        = "F", "Femaie"
+        OTHER         = "O", "Other"
+        PREFER_NOT_TO_SAY = "P", "Prefer not to say"
+
+    class Maritus_Status(models.TextChoices):
+        SELECT_YOUR_STATUS         = "", "Select your status"
+        SINGLE                     = "S", "Single"
+        MARRIED                    = "M", "Married"
+        DIVORCED                   = "D", "Divorced"
+        WIDOWED                    = "W", "Widowed"
+        SEPARATED                  = "SD", "Separated"
+        IN_A_RELATIONSHIP          = "I", "In a relationship"
+        ENGAGED                    = "E", "Engaged"
+        IN_A_CIVIL_PARTNERSHIP     = "IACP", "In a civil partnership"
+        IN_A_DOMESTIC_PARTNERSHIP  = "IADP", "In a domestic partnership"
+        ITS_COMPLICATED            = "IC", "It's complicated"
+        PREFER_NOT_TO_SAY          = "P", "Prefer not to say"
+
+    first_name     = models.CharField(max_length=40)
+    surname        = models.CharField(max_length=40)
+    email          = models.EmailField(max_length=40, unique=True)
+    mobile         = models.CharField(max_length=11)
+    country        = models.CharField(max_length=50)
+    state          = models.CharField(max_length=50)
+    postcode       = models.CharField(max_length=10)
+    gender         = models.CharField(choices=Gender.choices, max_length=1)
+    maritus_status = models.CharField(choices=Maritus_Status.choices, max_length=4)
+    
+
+
+class IdentificationDocument(models.Model):
+    class IdentificationType(models.TextChoices):
+        PASSPORT        = "p", "Passport"
+        DRIVING_LICENCE = "d", "Driving Licence"
+        NATIONAL_ID     = "n", "National ID"
+    
+    class Signature(models.TextChoices):
+        UPLOAD_SIGNATURE = "u", "Upload signature"
+        DRAW_SIGNATURE   = "d", "Draw signature"
+    
+    identification_type = models.CharField(choices=IdentificationType.choices, max_length=1)
+    signature           = models.CharField(choices=Signature.choices, max_length=1)
--- account/utils/utils.py
+++ account/utils/utils.py
@@ -2,5 +2,5 @@ from datetime import datetime
 
 
 def current_year_choices(number_of_years_to_create=20):
-    current_year = datetime().year
+    current_year = datetime.now().year
     return  [(year, str(year)) for year in range(current_year, current_year + number_of_years_to_create)]
